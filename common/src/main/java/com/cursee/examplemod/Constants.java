f1357bf28b5fa4657a8b0107fd35bb4e69bc8ed6
jason13official <135896906+jason13official@users.noreply.github.com>
2024-06-20 21:38:41 -0400

    Update Constants.java

--- common/src/main/java/com/cursee/examplemod/Constants.java
+++ common/src/main/java/com/cursee/examplemod/Constants.java
@@ -7,5 +7,7 @@ public class Constants {
 
 	public static final String MOD_ID = "examplemod";
 	public static final String MOD_NAME = "ExampleMod";
+	public static final String MOD_VERSION = "1.0.0";
+	public static final String MC_VERSION_RAW = "[1.20]";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
