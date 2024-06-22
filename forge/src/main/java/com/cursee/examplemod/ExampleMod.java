5133334ebb98149197279f2f5569a82912cbca01
jason13official <135896906+jason13official@users.noreply.github.com>
2024-06-22 12:48:16 -0400

    Update ExampleMod.java

--- forge/src/main/java/com/cursee/examplemod/ExampleMod.java
+++ forge/src/main/java/com/cursee/examplemod/ExampleMod.java
@@ -7,13 +7,6 @@ public class ExampleMod {
     
     public ExampleMod() {
     
-        // This method is invoked by the Forge mod loader when it is ready
-        // to load your mod. You can access Forge and Common code in this
-        // project.
-    
-        // Use Forge to bootstrap the Common mod.
-        Constants.LOG.info("Hello Forge world!");
         CommonClass.init();
-        
     }
 }
