194a2a89345083cd9dc81b9933062542ea864909
jason13official <jason13official@gmail.com>
2024-06-21 21:47:34 -0400

    monolib common and sailing register

--- common/src/main/java/com/cursee/examplemod/CommonClass.java
+++ common/src/main/java/com/cursee/examplemod/CommonClass.java
@@ -1,6 +1,7 @@
 package com.cursee.examplemod;
 
 import com.cursee.examplemod.platform.Services;
+import com.cursee.monolib.core.sailing.Sailing;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Items;
 
@@ -27,5 +28,7 @@ public class CommonClass {
 
             Constants.LOG.info("Hello to examplemod");
         }
+
+        Sailing.register(Constants.MOD_NAME, Constants.MOD_ID, Constants.MOD_VERSION, Constants.MC_VERSION_RAW, Constants.PUBLISHER_AUTHOR, Constants.PRIMARY_CURSEFORGE_MODRINTH);
     }
 }
