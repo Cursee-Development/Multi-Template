f930662e09ca6dfef2578d86b1d96b4d3701fe8a
jason13official <jason13official@gmail.com>
2024-07-03 13:35:59 -0400

    examplemod regression

---
+++ forge/src/main/java/com/example/examplemod/ExampleModForge.java
@@ -0,0 +1,12 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class ExampleModForge {
+    
+    public ExampleModForge() {
+    
+        ExampleModForge.init();
+    }
+}
---
+++ neoforge/src/main/java/com/example/examplemod/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package com.example.examplemod;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class ExampleModNeoForge {
+
+    public ExampleModNeoForge(IEventBus eventBus) {
+
+        ExampleModNeoForge.init();
+    }
+}
